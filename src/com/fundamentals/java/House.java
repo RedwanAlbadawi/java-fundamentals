0d402991a6949f575819c35fc74f6a66803939e2
Redwan Albadawi <redwan.albadawi@gmail.com>
2021-06-10 16:09:05 -0500

    Created House Class

---
+++ src/com/fundamentals/java/House.java
@@ -0,0 +1,14 @@
+package com.fundamentals.java;
+
+public class House {
+
+    String foundationType;
+    String roofStyle;
+    int WindowSize;
+    String doorColor;
+
+    public void doorOpenClose(){
+        System.out.println("The door is open");
+    }// end method
+
+}// end class
